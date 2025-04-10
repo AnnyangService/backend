a2c312c6b238df8ad6fa8db0dd9289fa0d4688fa
chisan ahn <chisan2001@gmail.com>
2025-04-10 17:47:25 +0900

    feat: 공통 응답 포맷을 위한 global/response 패키지 및 클래스 추가

---
+++ src/main/java/com/annyang/global/response/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.annyang.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final String code;
+    private final String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+}
