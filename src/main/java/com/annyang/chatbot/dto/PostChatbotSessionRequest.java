16c1c6dc0719f84bb653c015b42c0c054c9eccae
chisan ahn <chisan2001@gmail.com>
2025-08-26 20:48:15 +0900

    refactor: PostChatbotSessionRequest diagnosis_id 필드명 수정

--- src/main/java/com/annyang/chatbot/dto/PostChatbotSessionRequest.java
+++ src/main/java/com/annyang/chatbot/dto/PostChatbotSessionRequest.java
@@ -1,11 +1,16 @@
 package com.annyang.chatbot.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class PostChatbotSessionRequest {
+    
+    @JsonProperty("diagnosis_id")
     private String diagnosisId;
+
     private String query;
 }
